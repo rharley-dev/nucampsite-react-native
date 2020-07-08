325f941a074be6e4674d45f7fa904804d07da28e
Ryan Harley <rharley.dev@gmail.com>
2020-07-08 12:00:42 -0500

    implamented redux to components

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -8,7 +8,20 @@ import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-nativ
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import {
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+} from '../redux/ActionCreators';
 
+const mapDispatchToProps = {
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+};
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -118,19 +131,17 @@ const ContactNavigator = createStackNavigator(
 
 const CustomDrawerContentComponent = props => (
   <ScrollView>
-      <SafeAreaView 
-          style={styles.container}
-          forceInset={{top: 'always', horizontal: 'never'}}>
-          <View style={styles.drawerHeader}>
-              <View style={{flex: 1}}>
-                  <Image source={require('./images/logo.png')} style={styles.drawerImage} />
-              </View>
-              <View style={{flex: 2}}>
-                  <Text style={styles.drawerHeaderText}>NuCamp</Text>
-              </View>
-          </View>
-          <DrawerItems {...props} />
-      </SafeAreaView>
+    <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>NuCamp</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
   </ScrollView>
 );
 
@@ -173,11 +184,19 @@ const MainNavigator = createDrawerNavigator(
   },
   {
     drawerBackgroundColor: '#CEC8FF',
-    contentComponent: CustomDrawerContentComponent
+    contentComponent: CustomDrawerContentComponent,
   }
 );
 
 class Main extends Component {
+
+  componentDidMount() {
+    this.props.fetchCampsites();
+    this.props.fetchComments();
+    this.props.fetchPromotions();
+    this.props.fetchPartners();
+}
+
   render() {
     return (
       <View
@@ -194,7 +213,7 @@ class Main extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   drawerHeader: {
     backgroundColor: '#5637DD',
@@ -202,18 +221,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
-    flexDirection: 'row'
-},
-drawerHeaderText: {
+    flexDirection: 'row',
+  },
+  drawerHeaderText: {
     color: '#fff',
     fontSize: 24,
-    fontWeight: 'bold'
-},
-drawerImage: {
+    fontWeight: 'bold',
+  },
+  drawerImage: {
     margin: 10,
     height: 60,
-    width: 60
-},
+    width: 60,
+  },
   stackIcon: {
     marginLeft: 10,
     color: '#fff',
@@ -221,4 +240,4 @@ drawerImage: {
   },
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
