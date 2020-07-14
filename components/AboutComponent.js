23c93ac89629cfae6296132851b848680fc1be25
Ryan Harley <rharley.dev@gmail.com>
2020-07-14 09:36:52 -0500

    added animations

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
   return {
@@ -13,15 +14,18 @@ const mapStateToProps = state => {
 
 function Mission() {
   return (
-    <Card title="Our Mission">
-      <Text style={{ margin: 10 }}>
-        We present a curated database of the best campsites in the vast woods and backcountry of the
-        World Wide Web Wilderness. We increase access to adventure for the public while promoting
-        safe and respectful use of resources. The expert wilderness trekkers on our staff personally
-        verify each campsite to make sure that they are up to our standards. We also present a
-        platform for campers to share reviews on campsites they have visited with each other.
-      </Text>
-    </Card>
+    <Animatable.View animation="fadeInDown" duration={1000} delay={500}>
+      <Card title="Our Mission">
+        <Text style={{ margin: 10 }}>
+          We present a curated database of the best campsites in the vast woods and backcountry of
+          the World Wide Web Wilderness. We increase access to adventure for the public while
+          promoting safe and respectful use of resources. The expert wilderness trekkers on our
+          staff personally verify each campsite to make sure that they are up to our standards. We
+          also present a platform for campers to share reviews on campsites they have visited with
+          each other.
+        </Text>
+      </Card>
+    </Animatable.View>
   );
 }
 
@@ -54,10 +58,12 @@ class About extends Component {
     if (this.props.partners.errMess) {
       return (
         <ScrollView>
-          <Mission />
-          <Card title="Community Partners">
-            <Text>{this.props.partners.errMess}</Text>
-          </Card>
+          <Animatable.View animation="fadeInUp" duration={1000} delay={500}>
+            <Mission />
+            <Card title="Community Partners">
+              <Text>{this.props.partners.errMess}</Text>
+            </Card>
+          </Animatable.View>
         </ScrollView>
       );
     }
@@ -65,13 +71,15 @@ class About extends Component {
     return (
       <ScrollView>
         <Mission />
-        <Card title="Community Partners">
-          <FlatList
-            data={this.props.partners.partners}
-            renderItem={renderPartner}
-            keyExtractor={item => item.id.toString()}
-          />
-        </Card>
+        <Animatable.View animation="fadeInUp" duration={1000} delay={500}>
+          <Card title="Community Partners">
+            <FlatList
+              data={this.props.partners.partners}
+              renderItem={renderPartner}
+              keyExtractor={item => item.id.toString()}
+            />
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
