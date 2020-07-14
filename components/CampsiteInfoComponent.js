72fdceb779efbfcaa0577cf82640500a3bead42e
Ryan Harley <rharley.dev@gmail.com>
2020-07-14 15:59:50 -0500

    added rubberBand panResponder

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -32,10 +32,17 @@ const mapDispatchToProps = {
 function RenderCampsite(props) {
   const { campsite } = props;
 
-  const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+  const view = React.createRef();
+
+  const recognizeDrag = ({ dx }) => (dx < -100 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log('pan responder end', gestureState);
       if (recognizeDrag(gestureState)) {
@@ -65,8 +72,9 @@ function RenderCampsite(props) {
     return (
       <Animatable.View
         animation="fadeInDown"
-        duration={2000}
-        delay={1000}
+        duration={1000}
+        delay={500}
+        ref={view}
         {...panResponder.panHandlers}
       >
         <Card featuredTitle={campsite.name} image={{ uri: baseUrl + campsite.image }}>
@@ -117,7 +125,7 @@ function RenderComments({ comments }) {
   };
 
   return (
-    <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+    <Animatable.View animation="fadeInUp" duration={1000} delay={500}>
       <Card title="Comments">
         <FlatList
           data={comments}
