24e42d1dcc1f3eb5ecf5fda59cba17ccc751665f
Ryan Harley <rharley.dev@gmail.com>
2020-07-13 21:11:34 -0500

    delete favorite action

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -11,6 +13,8 @@ const mapStateToProps = state => {
     favorites: state.favorites,
   };
 };
+
+const mapDispatchToProps = { deleteFavorite: campsiteId => deleteFavorite(campsiteId) };
 
 class Favorites extends Component {
   static navigationOptions = {
@@ -20,13 +24,39 @@ class Favorites extends Component {
   render() {
     const { navigate } = this.props.navigation;
     const renderFavoriteItem = ({ item }) => {
+      const rightButton = [
+        {
+          text: 'Delete',
+          type: 'delete',
+          onPress: () => {
+            Alert.alert(
+              'Delete Favorite?',
+              'Are you sure you wish to delete the favorite campsite ' + item.name + '?',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log(item.name + 'Not Deleted'),
+                  style: ' cancel',
+                },
+                {
+                  text: 'OK',
+                  onPress: () => this.props.deleteFavorite(item.id),
+                },
+              ],
+              { cancelable: false }
+            );
+          },
+        },
+      ];
       return (
-        <ListItem
-          title={item.name}
-          subtitle={item.description}
-          leftAvatar={{ source: { uri: baseUrl + item.image } }}
-          onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-        />
+        <Swipeout right={rightButton} autoClose={true}>
+          <ListItem
+            title={item.name}
+            subtitle={item.description}
+            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+            onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+          />
+        </Swipeout>
       );
     };
 
@@ -52,4 +82,4 @@ class Favorites extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
