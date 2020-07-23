25e4d5a4aaebee505ecfb80ec4db412edcc16301
Ryan Harley <rharley.dev@gmail.com>
2020-07-23 13:30:26 -0500

    building expo network info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,7 +7,16 @@ import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
 import Login from './LoginComponent';
-import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import {
+  View,
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  Alert,
+  ToastAndroid,
+} from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
@@ -18,6 +27,7 @@ import {
   fetchPromotions,
   fetchPartners,
 } from '../redux/ActionCreators';
+import NetInfo from '@react-native-community/netinfo';
 
 const mapDispatchToProps = {
   fetchCampsites,
@@ -286,7 +296,7 @@ const MainNavigator = createDrawerNavigator(
     },
   },
   {
-    // initialRouteName: 'Home',
+    initialRouteName: 'Home',
     drawerBackgroundColor: '#CEC8FF',
     contentComponent: CustomDrawerContentComponent,
   }
@@ -298,7 +308,45 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromotions();
     this.props.fetchPartners();
+
+    // NetInfo.fetch().then(connectionInfo => {
+    //   Platform.OS === 'ios'
+    //     ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+    //     : ToastAndroid.show(
+    //         'Initial Network Connectivity Type: ' + connectionInfo.type,
+    //         ToastAndroid.LONG
+    //       );
+    // });
+
+    // this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+    //   this.handleConnectivityChange(connectionInfo);
+    // });
   }
+
+  // componentWillUnmount() {
+  //   this.unsubscribeNetInfo();
+  // }
+
+  // handleConnectivityChange = connectionInfo => {
+  //   let connectionMsg = 'You are now connected to an active network.';
+  //   switch (connectionInfo.type) {
+  //     case 'none':
+  //       connectionMsg = 'No network connection is active.';
+  //       break;
+  //     case 'unknown':
+  //       connectionMsg = 'The network connection state is now unknown.';
+  //       break;
+  //     case 'cellular':
+  //       connectionMsg = 'You are now connected to a cellular network.';
+  //       break;
+  //     case 'wifi':
+  //       connectionMsg = 'You are now connected to a WiFi network.';
+  //       break;
+  //   }
+  //   Platform.OS === 'ios'
+  //     ? Alert.alert('Connection change:', connectionMsg)
+  //     : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+  // };
 
   render() {
     return (
