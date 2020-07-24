8636b548381968a4c823896488a732df6d48e0a3
Ryan Harley <rharley.dev@gmail.com>
2020-07-23 21:39:45 -0500

    fixed NetInfo bug

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -309,44 +309,44 @@ class Main extends Component {
     this.props.fetchPromotions();
     this.props.fetchPartners();
 
-    // NetInfo.fetch().then(connectionInfo => {
-    //   Platform.OS === 'ios'
-    //     ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-    //     : ToastAndroid.show(
-    //         'Initial Network Connectivity Type: ' + connectionInfo.type,
-    //         ToastAndroid.LONG
-    //       );
-    // });
+    NetInfo.fetch().then(connectionInfo => {
+      Platform.OS === 'ios'
+        ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+        : ToastAndroid.show(
+            'Initial Network Connectivity Type: ' + connectionInfo.type,
+            ToastAndroid.LONG
+          );
+    });
 
-    // this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-    //   this.handleConnectivityChange(connectionInfo);
-    // });
+    this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+      this.handleConnectivityChange(connectionInfo);
+    });
   }
 
-  // componentWillUnmount() {
-  //   this.unsubscribeNetInfo();
-  // }
+  componentWillUnmount() {
+    this.unsubscribeNetInfo();
+  }
 
-  // handleConnectivityChange = connectionInfo => {
-  //   let connectionMsg = 'You are now connected to an active network.';
-  //   switch (connectionInfo.type) {
-  //     case 'none':
-  //       connectionMsg = 'No network connection is active.';
-  //       break;
-  //     case 'unknown':
-  //       connectionMsg = 'The network connection state is now unknown.';
-  //       break;
-  //     case 'cellular':
-  //       connectionMsg = 'You are now connected to a cellular network.';
-  //       break;
-  //     case 'wifi':
-  //       connectionMsg = 'You are now connected to a WiFi network.';
-  //       break;
-  //   }
-  //   Platform.OS === 'ios'
-  //     ? Alert.alert('Connection change:', connectionMsg)
-  //     : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
-  // };
+  handleConnectivityChange = connectionInfo => {
+    let connectionMsg = 'You are now connected to an active network.';
+    switch (connectionInfo.type) {
+      case 'none':
+        connectionMsg = 'No network connection is active.';
+        break;
+      case 'unknown':
+        connectionMsg = 'The network connection state is now unknown.';
+        break;
+      case 'cellular':
+        connectionMsg = 'You are now connected to a cellular network.';
+        break;
+      case 'wifi':
+        connectionMsg = 'You are now connected to a WiFi network.';
+        break;
+    }
+    Platform.OS === 'ios'
+      ? Alert.alert('Connection change:', connectionMsg)
+      : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+  };
 
   render() {
     return (
